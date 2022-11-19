a20f2450411585965b545088d7fe67ff0288a992
paburtor <114960234+paburtor@users.noreply.github.com>
2022-11-19 19:52:48 -0300

    Update

--- gradle.groovy
+++ gradle.groovy
@@ -7,28 +7,11 @@
 def call()
 {	
 stage('Build Gradle')
-  {            
-        steps 
-        { 
+ {            
             echo 'Building: Workspace -> [${env.WORKSPACE}]'                
             sh 'chmod -R 777 $WORKSPACE'
             sh './gradlew build'
-        }                                               
-
-        post 
-        {
-            success 
-            {
-                echo 'Build Success Gradle'
-                //slackSend color: "good", message: "Build Success"
-            }
-            failure 
-            {
-                echo 'Build Failed Gradle'
-                //slackSend color: "danger", message: "Build Failed"
-            }
-        }
-    }    
+  }    
 }
 
 return this;
